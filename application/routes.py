dfc53ad4939860e93c756a2e82c2c65d5c1ec3b2
Ezekiel Oluwasanjo <owaboyeooluwasanjo@yahoo.com>
2026-01-15 23:21:12 +0100

    Add user role

--- application/routes.py
+++ application/routes.py
@@ -10,14 +10,16 @@ routes_bp = Blueprint("routes", __name__)
 routes_bp.route('/')
 @login_required
 def dashboard():
+    
     inc_total = db.session.query(
         db.func.sum(IncomeExpTracker.amount)
-    ).filter(IncomeExpTracker.type == 'Income').scalar()
+    ).filter(IncomeExpTracker.type == 'Income',
+             IncomeExpTracker.id == session["user_id"]).scalar()
     
     cash_cat = db.session.query(
     IncomeExpTracker.type,
     db.func.sum(IncomeExpTracker.amount).label("Amount")
-    ).group_by(IncomeExpTracker.type).all()
+    ).filter(IncomeExpTracker.id == session["user_id"]).group_by(IncomeExpTracker.type).all()
 
     data = {row[0]: float(row[1]) for row in cash_cat}
 
@@ -25,24 +27,25 @@ def dashboard():
     income_by_Category = db.session.query(
         IncomeExpTracker.category,
         db.func.sum(IncomeExpTracker.amount).label("Amount")
-    ).filter(IncomeExpTracker.type == 'Income').group_by(IncomeExpTracker.category).all()
+    ).filter(IncomeExpTracker.type == 'Income', 
+             IncomeExpTracker.id == session["user_id"]).group_by(IncomeExpTracker.category).all()
 
     # Expenses by Category
     exp_by_Category = db.session.query(
         IncomeExpTracker.category,
         db.func.sum(IncomeExpTracker.amount).label("Amount")
-    ).filter(IncomeExpTracker.type == 'Expenses').group_by(IncomeExpTracker.category).all()
+    ).filter(IncomeExpTracker.type == 'Expenses', IncomeExpTracker.id == session["user_id"]).group_by(IncomeExpTracker.category).all()
 
     monthly_expenses = db.session.query(
         db.func.strftime('%Y-%m', IncomeExpTracker.created_at).label("month"),
         db.func.sum(IncomeExpTracker.amount).label("total")
-    ).filter(IncomeExpTracker.type == 'Expenses').group_by("month").all()
+    ).filter(IncomeExpTracker.type == 'Expenses', IncomeExpTracker.id == session["user_id"]).group_by("month").all()
 
     
     monthly_income = db.session.query(
         db.func.strftime('%Y-%m', IncomeExpTracker.created_at).label("month"),
         db.func.sum(IncomeExpTracker.amount).label("total")
-    ).filter(IncomeExpTracker.type == 'Income').group_by("month").all()
+    ).filter(IncomeExpTracker.type == 'Income', IncomeExpTracker.id == session["user_id"]).group_by("month").all()
 
     return render_template('dashboard.html', title='Dashboard', 
                            inc_total=inc_total, 
@@ -56,11 +59,11 @@ def dashboard():
 routes_bp.route('/get_cashflow')
 @login_required
 def get_cashflow():
-    entries = IncomeExpTracker.query.order_by(IncomeExpTracker.created_at.desc()).all()
+    entries = IncomeExpTracker.query.filter(IncomeExpTracker.id == session["user_id"]).order_by(IncomeExpTracker.created_at.desc()).all()
     cash_cat = db.session.query(
     IncomeExpTracker.type,
     db.func.sum(IncomeExpTracker.amount).label("Amount")
-    ).group_by(IncomeExpTracker.type).all()
+    ).filter(IncomeExpTracker.id == session["user_id"]).group_by(IncomeExpTracker.type).all()
 
     data = {row[0]: float(row[1]) for row in cash_cat}
     return render_template('cashflow/index.html', entries=entries, data=data, title='All entries')
@@ -69,6 +72,8 @@ routes_bp.route('/edit_cashflow/<int:id>', methods=['GET', 'POST'])
 @login_required
 def edit_cashflow(id):
     record = IncomeExpTracker.query.get_or_404(id)
+    if record.user_id != session["user_id"] and session["role"] != "admin":
+        abort(403)
     form = IncomeExpForm(obj=record)
     if form.validate_on_submit():
         record.amount = form.amount.data
@@ -141,7 +146,7 @@ def monthly_income_items(month):
     items = db.session.query(IncomeExpTracker).filter(
         IncomeExpTracker.type == 'Income',
         db.func.strftime('%Y-%m', IncomeExpTracker.created_at) == month
-    ).all()
+    ).filter(IncomeExpTracker.id == session["user_id"]).all()
     total = sum( item.amount for item in items)
     return render_template("cashflow/monthly_income_items.html", items=items, month=month, total=total)
 
@@ -151,7 +156,7 @@ def monthly_expenses_items(month):
     items = db.session.query(IncomeExpTracker).filter(
         IncomeExpTracker.type == 'Expenses',
         db.func.strftime('%Y-%m', IncomeExpTracker.created_at) == month
-    ).all()
+    ).filter(IncomeExpTracker.id == session["user_id"]).all()
     total = sum( item.amount for item in items)
     return render_template("cashflow/monthly_expenses_items.html", items=items, month=month, total=total)
 
@@ -175,6 +180,7 @@ def sign_in():
         session['authenticated'] = True
         session['user_id'] = user.id
         session['email'] = user.email
+        session["role"] = user.role
         session['full_name'] = f"{user.first_name} {user.last_name}"
 
         return redirect(url_for('dashboard'))
